e8607d695566f5159260a865978a31ed27d20a44
SidVaity <sidvaity@gmail.com>
2025-04-26 21:32:45 +0530

    Setup repo

---
+++ src/main/java/com/backend/distributedqueue/DistributedqueueApplication.java
@@ -0,0 +1,13 @@
+package com.backend.distributedqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedqueueApplication.class, args);
+	}
+
+}
