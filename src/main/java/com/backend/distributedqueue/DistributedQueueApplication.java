ce9f53feb69fa3d4215d981a4da6509a2ea405cd
SidheshV <vaity.sidhesh.18it2008@gmail.com>
2025-05-24 20:47:56 +0530

    Adding the proto locally

--- src/main/java/com/backend/distributedqueue/DistributedQueueApplication.java
+++ src/main/java/com/backend/distributedqueue/DistributedQueueApplication.java
@@ -1,11 +1,12 @@
 package com.backend.distributedqueue;
 
+import com.shared.protosnomo.Job;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DistributedqueueApplication {
+public class DistributedQueueApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DistributedqueueApplication.class, args);
+        SpringApplication.run(DistributedQueueApplication.class, args);
     }
 }
---
+++ src/main/java/com/backend/distributedqueue/orchestrator/JobOrchestrator.java
@@ -0,0 +1,9 @@
+package com.backend.distributedqueue.orchestrator;
+
+import com.shared.protosnomo.DataPopulationPayload;
+import com.shared.protosnomo.EmailPayload;
+import com.shared.protosnomo.Job;
+
+public class JobOrchestrator {
+
+}
